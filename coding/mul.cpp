320b8838d7122511a3c0edae02eb7cf741d8800d
Chayan-1 <159713253+Chayan-1@users.noreply.github.com>
2024-02-11 23:13:40 +0530

    Updated mul.cpp using function

--- coding/mul.cpp
+++ coding/mul.cpp
@@ -1,7 +1,10 @@
 // Multiplication of two numbers:
 #include <iostream>
 using namespace std;
-
+int mul(int a, int b)
+{
+    return a*b;
+}
 int main() {
     int a, b;
     cout << "Enter first number  : ";
@@ -9,6 +12,6 @@ int main() {
     cout << "Enter second number : ";
     cin >> b;
     
-    int result = a * b;
+    int result =mul(a,b);
     cout << "Multiplication : " << result << endl;
 }
