c2d5dc082e9387c39fa8f8bc6f90b81470d1b16b
Rishikesh <rishikeshpanditrp@gmail.com>
2024-02-11 18:45:16 +0530

    addition fn

--- coding/add.cpp
+++ coding/add.cpp
@@ -1,13 +1,21 @@
-// Addition of two numbers
+/// Addition of two numbers
 #include<iostream>
 using namespace std;
 
+// Addition of two numbers
 int Add(int a, int b){
     return (a+b);
 }
 
+// complete this
+int Add(int a, int b, int c){
+    // Add your code
+    return (a+b+c);
+}
+
 int main(){
-    int x,y;
-    cin>>x>>y;
+    int x,y,z;
+    cin>>x>>y>>z;
     cout<<Add(x,y);
+    // cout<<" "<<Add(x,y,z);
 }
