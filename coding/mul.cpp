87df103496306a57356e4075e2b97f44893ea365
mdarifaftab <155831515+mdarifaftab@users.noreply.github.com>
2024-02-11 21:45:10 +0530

    Arif update multi

--- coding/mul.cpp
+++ coding/mul.cpp
@@ -1,1 +1,14 @@
 // Multiplication of two numbers:
+include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Enter first number  : ";
+    cin >> a;
+    cout << "Enter second number : ";
+    cin >> b;
+    
+    int result = a * b;
+    cout << "Multiplication : " << result << endl;
+}
